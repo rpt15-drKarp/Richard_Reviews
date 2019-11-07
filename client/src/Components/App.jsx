0124c0ea30b46210a55e2c2c4e0b21fa27f7c89c
Richard Kang <richardkang1121@gmail.com>
2019-11-06 22:58:06 -0600

    Edit app to call to correct ip address

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -17,7 +17,7 @@ class App extends React.Component {
   }
 
   componentDidMount () {
-    axios.get(`http://localhost:3001/api/reviews/${this.state.gameId}`)
+    axios.get(`/api/reviews/${this.state.gameId}`)
     .then((data) => {
       this.setState({
         reviews: data.data
