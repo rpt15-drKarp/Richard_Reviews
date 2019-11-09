f07ef790a1de5f5fd4d62ca2ee223d52f473b2a3
Richard Kang <richardkang1121@gmail.com>
2019-11-09 16:21:16 -0600

    Append / to loaderio url

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get(`/loaderio-0cbeca2a61656a04739e2f907fadabe7`, (req, res) => {
+app.get(`/loaderio-0cbeca2a61656a04739e2f907fadabe7/`, (req, res) => {
   res.sendFile(path.join(__dirname, '../public/loaderio-0cbeca2a61656a04739e2f907fadabe7.txt'));
 })
 
